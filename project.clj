0d5fa3fcff17d4d83cb207d0cdbe4f012f9a1739
David Thomas Hume <dthume@gmail.com>
2010-02-06 18:28:19 +0000

    Updated clojure version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.dthume/util "1.0.0-SNAPSHOT"
   :description "Core Clojure Utilities"
   :dependencies
-  [[org.clojure/clojure "1.1.0-new-SNAPSHOT"]
-   [org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
+  [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+   [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]])
